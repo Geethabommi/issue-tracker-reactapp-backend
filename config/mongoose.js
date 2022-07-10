cbedc5d493927837b4ab994901bf246ae7ec14f9
Geethabommi <geethabommi139@gmail.com>
2022-07-10 20:22:41 +0530

    env

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
 
-
-mongoose.connect('mongodb://localhost/issues-tracker');
-
+// mongoose.connect('mongodb://localhost/issues-tracker');
+mongoose.connect(process.env.MONGO_URL);
 
 const db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'Error connecting to monodb'));
 
-db.on('error', console.error.bind(console, "Error connecting to monodb"));
-
-db.once('open', function(){
-    console.log('Connected to database ::  MongoDB');
+db.once('open', function () {
+  console.log('Connected to database ::  MongoDB');
 });
 
 module.exports = db;
